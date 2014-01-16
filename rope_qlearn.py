5be24910b1f0134ea69ff35d8129372a250601ea
Sandy Huang <sandyhhuang@gmail.com>
2014-01-16 14:51:06 -0800

    Sandy/Dylan added computation of shape context features in frame of gripper. Redid caching for warping hmats.

--- rope_qlearn.py
+++ rope_qlearn.py
@@ -26,6 +26,14 @@ except:
     use_rapprentice = False
 
 DS_SIZE = .025
+GRIPPER_OPEN_CLOSE_THRESH = 0.4
+
+# constants for shape context
+R_MIN, P_MIN, T_MIN = 0.1, 1.3, -0.5
+R_MAX, P_MAX, T_MAX = 0.15, 2.2, 0.5
+R_BINS, P_BINS, T_BINS = 2, 4, 6
+DENSITY_RADIUS = .2
+
 
 # rip rope_max_margin_model, you will not be missed --eric 1/14/2014
 
@@ -59,6 +67,26 @@ def add_constraints_from_demo(mm_model, expert_demofile, outfile=None, verbose=F
         if outfile:
             mm_model.save_constraints_to_file(outfile)
 
+def concatenate_fns(fns, actionfile):
+    if type(actionfile) is str:
+        actionfile = h5py.File(actionfile, 'r')
+    
+    fn_params = [f(actionfile) for f in fns]
+    def result_fn(state, action):
+        results = np.array([])
+        for (f, _, _) in fn_params:
+            results = np.r_[results, f(state, action)]
+        return results
+
+    N = sum(v[1] for v in fn_params)
+    return (result_fn, N, actionfile)
+
+def get_sc_feature_fn(actionfile):
+    if type(actionfile) is str:
+        actionfile = h5py.File(actionfile, 'r')
+    act_set = ActionSet(actionfile)
+    return (act_set.sc_features, act_set.num_sc_features, actionfile)    
+
 def get_bias_feature_fn(actionfile):
     if type(actionfile) is str:
         actionfile = h5py.File(actionfile, 'r')
@@ -86,23 +114,51 @@ def get_action_state_margin_fn(actionfile):
 class ActionSet(object):
     """
     class to handle computing features and margins for state/action pairs
+
+
+    state is assumed to be a list [<state_id>, <point_cloud>]
     """
     def __init__(self, actionfile, use_cache = True):
         self.actionfile = actionfile
         self.actions = sorted(actionfile.keys())
         self.action_to_ind = dict((v, i) for i, v in enumerate(self.actions))
         self.num_actions = len(self.actions)
-        self.action_margin_cache = {}
-        self.action_state_cache = {}
+        self.num_sc_features = R_BINS*T_BINS*P_BINS*2
+        self.cache = {}
         self.use_cache = use_cache
         self.link_names = ["%s_gripper_tool_frame"%lr for lr in ('lr')]
 
+    def _warp_hmats(self, state, action):
+        hit, value = self.check_cache(state, action)
+        if hit:
+            return value
+        else:
+            [warped_trajs, rc] = warp_hmats(self.get_ds_cloud(action),
+                                      state[1],
+                                      [(lr, self.actionfile[action][ln]['hmat']) for lr, ln in zip('lr', self.link_names)])
+            self.store_cache(state, action, [warped_trajs, rc])
+            return [warped_trajs, rc]
+
     def get_ds_cloud(self, action):
         return clouds.downsample(self.actionfile[action]['cloud_xyz'], DS_SIZE)
+
+    def sc_features(self, state, action):
+        seg_info = self.actionfile[action]
+
+        warped_trajs, _ = self._warp_hmats(state, action)
+        feat_val = dict((lr, np.zeros(self.num_sc_features/2.0)) for lr in 'lr')
+        for lr in 'lr':
+            grip = np.asarray(seg_info[lr + '_gripper_joint'])
+            closings = np.flatnonzero((grip[1:] < GRIPPER_OPEN_CLOSE_THRESH) & (grip[:-1] >= GRIPPER_OPEN_CLOSE_THRESH))
+            if closings:
+                first_close = closings[0]
+                close_hmat = warped_trajs[lr][first_close]
+                feat_val[lr] = gripper_frame_shape_context(state[1], close_hmat)
+        return np.r_[feat_val['l'], feat_val['r']]            
     
     def bias_features(self, state, action):
         feat = np.zeros(self.num_actions + 1)
-        feat[0] = registration_cost(state[1], self.get_ds_cloud(action))
+        (_, feat[0]) = self._warp_hmats(state, action)
         feat[self.action_to_ind[action]+1] = 1
         return feat
     
@@ -120,49 +176,31 @@ class ActionSet(object):
         warp both actions, compare the resulting trajectories:
         ex. warp a1 -> a2; use compare_hmats(warp(a1.traj), a2.traj)
         """
-        (cache_hit, value) = self.check_am_cache(a1, a2)
-        if cache_hit:
-            print 'action_only_margin_cache hit'
-            return value
-        warped_a1_trajs = [warp_hmats(self.get_ds_cloud(a1),
-                                      self.get_ds_cloud(a2),
-                                      self.actionfile[a1][ln]['hmat'][:]) for ln in self.link_names]
+        warped_a1_trajs = self._warp_hmats((a2, self.get_ds_cloud(a2)), a1)
+        warped_a1_trajs = [warped_a1_trajs[lr] for lr in 'lr']
         a1_trajs = [self.actionfile[a1][ln]['hmat'][:] for ln in self.link_names]
-        warped_a2_trajs = [warp_hmats(self.get_ds_cloud(a2),
-                                      self.get_ds_cloud(a1),
-                                      self.actionfile[a2][ln]['hmat'][:]) for ln in self.link_names]
+        warped_a2_trajs = self._warp_hmats((a1, self.get_ds_cloud(a1)), a2)
+        warped_a2_trajs = [warped_a2_trajs[lr] for lr in 'lr']
         a2_trajs = [self.actionfile[a2][ln]['hmat'][:] for ln in self.link_names]
         ret_val = sum(compare_hmats(t1, t2) for (t1, t2) in 
                           zip(warped_a1_trajs + warped_a2_trajs, a2_trajs + a1_trajs))
-        self.am_cache_store(a1, a2, ret_val)
         return ret_val
 
-    def check_am_cache(self, a1, a2):
-        if self.use_cache:
-            for key in [(a1, a2), (a2, a1)]:
-                if key in self.action_margin_cache:
-                    return (True, self.action_margin_cache[key])
-        return False, False
-
-    def am_cache_store(self, a1, a2, v):
-        if self.use_cache:
-            self.action_margin_cache[(a1, a2)] = v
-
-    def check_asm_cache(self, s, a):
+    def check_cache(self, s, a):
         # cache stores the warped traj for a
         if self.use_cache:
             key = (s[0], a)
-            if key in self.action_state_cache:
-                return (True, self.action_state_cache[key])
+            if key in self.cache:
+                return (True, self.cache[key])
         return False, False
 
-    def store_asm_cache(self, s, a, v):
+    def store_cache(self, s, a, v):
         if self.use_cache:
             key = (s[0], a)
-            self.action_state_cache[key] = v
+            self.cache[key] = v
 
-    def clear_asm_cache(self):
-        self.action_state_cache = {}
+    def clear_cache(self):
+        self.cache = {}
 
     def action_state_margin(self, state, a1, a2):
         """
@@ -174,27 +212,77 @@ class ActionSet(object):
         when we call this with a particular expert demo we will warp that trajectory
         once for each action we compare to -- issue is hashing point clouds effectively        
         """
-        a1_hit, value = self.check_asm_cache(state, a1)
-        if a1_hit:
-            warped_a1_trajs = value
-        else:
-            warped_a1_trajs = [warp_hmats(self.get_ds_cloud(a1),
-                                          state[1],
-                                          self.actionfile[a1][ln]['hmat']) for ln in self.link_names]
-            self.store_asm_cache(state, a1, warped_a1_trajs)
-        a2_hit, value = self.check_asm_cache(state, a2)
-        if a2_hit:
-            warped_a2_trajs = value
-        else:
-            warped_a2_trajs = [warp_hmats(self.get_ds_cloud(a2),
-                                      state[1],
-                                      self.actionfile[a2][ln]['hmat']) for ln in self.link_names]
-            self.store_asm_cache(state, a2, warped_a2_trajs)
-        return sum(compare_hmats(t1, t2) for (t1, t2) in zip(warped_a1_trajs, warped_a2_trajs))
+        warped_a1_trajs, _ = self._warp_hmats(state, a1)
+        warped_a2_trajs, _ = self._warp_hmats(state, a2)
+        return sum(compare_hmats(warped_a1_trajs[lr], warped_a2_trajs[lr]) for lr in 'lr')
 
     def combined_margin(self, state, a1, a2):
         return (self.action_only_margin(a1, a2) +
                 self.action_state_margin(a1, a2, state))
+
+
+
+#http://stackoverflow.com/questions/4116658/faster-numpy-cartesian-to-spherical-coordinate-conversion
+def cart2spherical(xyz):
+    xyz = np.asarray(xyz)
+    ptsnew =  np.zeros(xyz.shape)
+    xy = xyz[:,0]**2 + xyz[:,1]**2
+    ptsnew[:,0] = np.sqrt(xy + xyz[:,2]**2)
+    ptsnew[:,1] = np.arctan2(xyz[:,1], xyz[:,0])
+    ptsnew[:,2] = np.arctan2(np.sqrt(xy), xyz[:,2]) # for elevation angle defined from Z-axis down
+    return ptsnew
+
+def gripper_frame_shape_context(xyz, hmat):
+    h_inv = np.linalg.inv(hmat)
+    xyz1 = np.ones((len(xyz),4),'float')  #homogeneous coord
+    xyz1[:,:3] = xyz
+    xyz2 = [np.dot(h_inv, pt)[:3] for pt in xyz1] #bestpractices
+    xyz3 = cart2spherical(xyz2)
+    bin_volumes = {}
+    bin_weights = {}
+
+    kd = sp_spat.KDTree(xyz)
+
+    rvals = np.linspace(R_MIN, R_MAX, R_BINS+1)
+    tvals = np.linspace(T_MIN, T_MAX, T_BINS+1)
+    pvals = np.linspace(P_MIN, P_MAX, P_BINS+1)
+
+    rbin_width = rvals[1] - rvals[0]
+    tbin_width = tvals[1] - tvals[0]
+    pbin_width = pvals[1] - pvals[0]
+
+    for (pt_i, (rho, theta, phi)) in enumerate(xyz3):
+        # Get point density: # of points in a sphere of radius DENSITY_RADIUS around this point
+        pt_density = len(kd.query_ball_point(xyz[pt_i], DENSITY_RADIUS))
+
+        # Find bin indices for point - correspond to the lower limits of the
+        # bins that the point is in
+        if rho > R_MAX or rho < R_MIN or theta > T_MAX or theta < T_MIN \
+                or phi > P_MAX or phi < P_MIN: continue
+        r_ind = math.floor((rho-rvals[0]) / rbin_width)
+        t_ind = math.floor((theta-tvals[0]) / tbin_width)
+        p_ind = math.floor((phi-pvals[0]) / pbin_width)
+
+        # Calculate bin volume if it has not been calculated yet
+        if (r_ind, t_ind, p_ind) not in bin_volumes:
+            bin_volumes[(r_ind, t_ind, p_ind)] = get_sect_vol(
+                    (rvals[r_ind], rvals[r_ind+1]),
+                    (tvals[t_ind], tvals[t_ind+1]),
+                    (pvals[p_ind], pvals[p_ind+1]))
+
+        # Update bin weight
+        orig_weight = 0
+        if (r_ind, t_ind, p_ind) in bin_weights:
+            orig_weight = bin_weights[(r_ind, t_ind, p_ind)]
+        new_pt_weight = 1 / (pt_density * bin_volumes[(r_ind, t_ind, p_ind)]**(1/3.0))
+        bin_weights[(r_ind, t_ind, p_ind)] = orig_weight + new_pt_weight
+
+    sc_features = np.zeros(R_BINS * T_BINS * P_BINS)
+    for (r, p, t) in bin_weights.iterkeys():
+        sc_features[r*T_BINS*P_BINS + p*T_BINS + t] = bin_weights[(r, p, t)]
+    return sc_features
+
+
 def extract_point(hmat):
     return hmat[:3, 3]
 
@@ -222,16 +310,20 @@ def compare_hmats(traj1, traj2):
             DTW[i, j] = hmat_cost(hmat1, hmat2) + best_next
     return DTW[n, m]
 
-def warp_hmats(xyz_src, xyz_targ, hmats):
+def warp_hmats(xyz_src, xyz_targ, hmat_list):
     if not use_rapprentice:
-        return hmats
+        return hmat_list
     scaled_xyz_src, src_params = registration.unit_boxify(xyz_src)
     scaled_xyz_targ, targ_params = registration.unit_boxify(xyz_targ)        
-    f,_ = registration.tps_rpm_bij(scaled_xyz_src, scaled_xyz_targ, plot_cb = None,
+    f,g = registration.tps_rpm_bij(scaled_xyz_src, scaled_xyz_targ, plot_cb = None,
                                    plotting=0,rot_reg=np.r_[1e-4,1e-4,1e-1], 
                                    n_iter=50, reg_init=10, reg_final=.1)
+    cost = registration.tps_reg_cost(f) + registration.tps_reg_cost(g)
     f = registration.unscale_tps(f, src_params, targ_params)
-    return f.transform_hmats(hmats)
+    trajs = {}
+    for k, hmats in hmat_list:
+        trajs[k] = f.transform_hmats(hmats)
+    return [trajs, cost]
 
 def get_downsampled_clouds(demofile):
     if not use_rapprentice:
@@ -240,15 +332,6 @@ def get_downsampled_clouds(demofile):
 
 def get_clouds(demofile):
     return [seg["cloud_xyz"] for seg in demofile.values()]
-
-def registration_cost(xyz0, xyz1):
-    if not use_rapprentice:
-        return 1
-    scaled_xyz0, _ = registration.unit_boxify(xyz0)
-    scaled_xyz1, _ = registration.unit_boxify(xyz1)
-    f,g = registration.tps_rpm_bij(scaled_xyz0, scaled_xyz1, rot_reg=1e-3, n_iter=10)
-    cost = registration.tps_reg_cost(f) + registration.tps_reg_cost(g)
-    return cost
 
 def combine_expert_demo_files(infile1, infile2, outfile):
     """
@@ -297,6 +380,10 @@ def select_feature_fn(args):
     if args.quad_features:
         print 'Using quadratic features.'
         feature_fn, num_features, act_file = get_quad_feature_fn(args.actionfile)
+    elif args.sc_features:
+        print 'Using sc and bias features.'
+        fns = [get_bias_feature_fn, get_sc_feature_fn]
+        feature_fn, num_features, act_file = concatenate_fns(fns, args.actionfile)
     else:
         print 'Using bias features.'
         feature_fn, num_features, act_file = get_bias_feature_fn(args.actionfile)
@@ -341,6 +428,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
     parser.add_argument("--quad_features", action="store_true")
+    parser.add_argument("--sc_features", action="store_true")
     parser.add_argument('--C', '-c', type=float, default=1)
     parser.add_argument("--multi_slack", action="store_true")
 
