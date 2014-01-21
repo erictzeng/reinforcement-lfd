66c47bc4d60fd2656d1c5805d4245d654ad5cc9f
Alex Lee <alexleegk@gmail.com>
2014-01-20 19:27:22 -0800

    Finally add a script that print the number of keys in an h5 file

---
+++ check_h5_length.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import argparse
+import h5py
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('h5file', type=str)
+    args = parser.parse_args()
+    
+    f = h5py.File(args.h5file, 'r')
+    print "File %s has %i keys"%(args.h5file, len(f.keys()))
+    f.close()
