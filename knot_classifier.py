47613cd753cb30001ebeb8dec325e6edc99970d0
Eric Tzeng <eric.s.tzeng@gmail.com>
2014-01-27 00:55:07 -0800

    removing dependencies that are knot needed anymore for knot_classifier (ohohoho good pun)

--- knot_classifier.py
+++ knot_classifier.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 
-import argparse
-import h5py
-import matplotlib.pyplot as plt
-import pylab
-import colorsys
 import numpy as np
-import IPython as ipy
-import os
 
 #
 # line segment intersection using vectors
@@ -79,45 +72,3 @@ def isKnot(rope_nodes):
         if flipped_top[::-1] in s:
             return True
     return False
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("results_file", type=str)
-    parser.add_argument("knot_images_dir", nargs='?', type=str, default=None)
-    parser.add_argument("not_images_dir", nargs='?', type=str, default=None)
-    args = parser.parse_args()
-    
-    result_file = h5py.File(args.results_file, 'r')
-
-    if not args.knot_images_dir:
-        args.knot_images_dir = os.path.join(os.path.dirname(args.results_file), 'knot_images')
-        if not os.path.exists(args.knot_images_dir):
-            os.makedirs(args.knot_images_dir)
-    
-    if not args.not_images_dir:
-        args.not_images_dir = os.path.join(os.path.dirname(args.results_file), 'not_images')
-        if not os.path.exists(args.not_images_dir):
-            os.makedirs(args.not_images_dir)
-    
-    for i_task, task_info in result_file.iteritems():
-        for i_step, step_info in task_info.iteritems():
-            try:
-                rope_nodes = step_info['rope_nodes'][()]
-            except:
-                rope_nodes = step_info
-            plt.clf()
-            plt.cla()
-
-            links_z = (rope_nodes[:-1,2] + rope_nodes[1:,2])/2.0
-            for i in np.argsort(links_z):
-                f = float(i)/(links_z.shape[0]-1)
-                plt.plot(rope_nodes[i:i+2,0], rope_nodes[i:i+2,1], c=colorsys.hsv_to_rgb(f,1,1), linewidth=6)
-            plt.axis("equal")
-            
-            if isKnot(rope_nodes):
-                fname = os.path.join(args.knot_images_dir, "task_%s_step_%s" % (str(i_task), str(i_step)))
-                plt.savefig(fname)
-            else:
-                fname = os.path.join(args.not_images_dir, "task_%s_step_%s" % (str(i_task), str(i_step)))
-                plt.savefig(fname)
-            print "saved ", fname
