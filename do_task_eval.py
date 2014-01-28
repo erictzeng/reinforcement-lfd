fixed lookahead so that it doesn't do unnecessary value fn computations, adding in updates to subselect_features for quad_landmark

do_task_eval.py
@@ -569,10 +569,10 @@ if __name__ == "__main__":
                 for (i_lookahead, action) in zip(range(len(best_actions)), best_actions):
                     redprint("looking ahead, depth 1: %i/%i\r"%(i_lookahead+1,args.lookahead_branches))
                     set_rope_transforms(start_rope_tfs)
-                    success, bodypart2trajs = simulate_demo(new_xyz, actionfile[action], animate=args.animation)
+                    success, bodypart2trajs = simulate_demo(new_xyz, actionfile[action], animate=False)
+                    if args.animation:
+                        Globals.viewer.Step()
                     next_xyz = Globals.sim.observe_cloud()
-                    if is_knot(next_xyz):
-                        knot_action_ind = i_lookahead
 
                     next_state = ("eval_%i"%get_unique_id(), next_xyz)
                     if not args.twostep_lookahead:
@@ -580,6 +580,10 @@ if __name__ == "__main__":
                     level1_states.append(next_xyz)
                     trajectories.append(bodypart2trajs)
                     end_rope_tfs.append(get_rope_transforms())
+
+                    if is_knot(next_xyz):
+                        knot_action_ind = i_lookahead
+                        break
 
                     if args.twostep_lookahead:
                         next_q_values = [q_value_fn(next_state[:], action) for action in actions]
@@ -609,12 +613,18 @@ if __name__ == "__main__":
                             redprint("looking ahead, depth 2: %i/%i\r"%(level2_i+1, args.lookahead_brances))
                             level1_i = level2_action[0]  # Index of action taken in level 1 of lookahead
                             set_rope_transforms(end_rope_tfs[level1_i])
-                            success, bodypart2trajs = simulate_demo(level1_states[level1_i], actionfile[level2_action[1]], animate=args.animation)
+                            success, bodypart2trajs = simulate_demo(level1_states[level1_i], actionfile[level2_action[1]], animate=False)
+                            if args.animation:
+                                Globals.viewer.Step()
 
                             next_state = ("eval_%i"%get_unique_id(), Globals.sim.observe_cloud())
+                            if is_knot(next_state[1]):
+                                state_values.append(np.inf)
+                                break
                             state_values.append(value_fn(next_state))
-                                
-                        best_action_ind = level2_best_actions[np.argmax(state_values)][0]
+
+                        
+                        best_action_ind = level2_best_action_inds[np.argmax(state_values)][0]
 
                 best_action = best_actions[best_action_ind]
                 if args.animation: