3e851a9b40de858b6551869b885f1f7cc76643a3
Sandy Huang <sandyhhuang@gmail.com>
2014-05-08 16:02:32 -0700

    Structure for registrations.py

---
+++ rapprentice/registrations.py
@@ -0,0 +1,36 @@
+
+
+
+# Function containing simulated annealing outer loop
+# Parameters:
+#     em_step_fcn
+#     x_nd
+#     y_md
+#     n_iter
+#     lambda
+#     T_init
+#     T_final
+#     rot_reg
+#     vis_cost
+
+
+# Function for TPS-RPM (as described in Chui et al.), with and w/o visual
+# features.
+# Parameters:
+#     x_nd
+#     y_md
+#     lambda
+#     T
+#     rot_reg
+#     vis_cost - function that returns cost btwn two points, based on visual features
+
+
+# Function for Reg4 (as described in Combes and Prima), with and w/o visual
+# features.
+# Parameters:
+#     x_nd
+#     y_md
+#     lambda
+#     T
+#     rot_reg
+#     vis_cost
