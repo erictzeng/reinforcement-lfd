0a476db48fdd3ca1b43c59538fcd1f4f363e273e
Alex Lee <alexleegk@gmail.com>
2014-05-15 14:49:43 -0700

    Add another experiment in compare_registrations and refactor plotting callback generator

--- compare_registrations.py
+++ compare_registrations.py
@@ -26,10 +26,41 @@ def downsample_cloud(cloud):
     cloud_xyzrgb_downsamp = np.zeros((new_n,d+3))
     cloud_xyzrgb_downsamp[:,:d] = cloud_xyz_downsamp
     cloud_xyzrgb_downsamp[:,d:] = cloud[min_indices,d:]
-    print cloud_xyzrgb_downsamp.shape
     return cloud_xyzrgb_downsamp
 
+def plot_cb_gen(output_prefix, args, x_color, y_color):
+    def plot_cb(x_nd, y_md, corr_nm, f, iteration):
+        if args.plot_color:
+            registrations.plot_callback(x_nd, y_md, corr_nm, f, iteration, output_prefix, x_color = x_color, y_color = y_color, proj_2d=args.proj)
+        else:
+            registrations.plot_callback(x_nd, y_md, corr_nm, f, iteration, output_prefix, proj_2d=args.proj)
+    return plot_cb
+
 def run_experiments(args):
+    if 0 in args.experiments:
+        run_experiments0(args)
+    if 1 in args.experiments:
+        run_experiments1(args)
+
+def run_experiments1(args):
+    infile = h5py.File(args.input_file)
+    for i in range(len(infile)):
+        source_cloud = downsample_cloud(infile[str(i)]['source_cloud'][()])
+        
+        for (cloud_key, target_cloud) in infile[str(i)]['target_clouds'].iteritems():
+            target_cloud = downsample_cloud(target_cloud[()])
+            print "source cloud %d and target cloud %s"%(i, cloud_key)
+            
+            vis_costs_xy = registrations.ab_cost(source_cloud, target_cloud)
+    
+            f, bend_cost, res_cost, total_cost = registrations.sim_annealing_registration(source_cloud[:,:-3], target_cloud[:,:-3],
+                registrations.rpm_em_step, vis_cost_xy = vis_costs_xy,
+                plotting=1, plot_cb = plot_cb_gen(os.path.join(args.output_folder, k + "_" + cloud_key + "_rpmvis") if args.output_folder else None,
+                                                  args,
+                                                  source_cloud[:,-3:],
+                                                  target_cloud[:,-3:]))
+
+def run_experiments0(args):
     clouds = h5py.File(args.input_file)
     _,d = clouds['dem_cloud'][()].shape
     d = d - 3  # ignore the RGB values
@@ -54,18 +85,13 @@ def run_experiments(args):
         y_md = y_xyzrgb[:,:d]
         vis_costs_xy = registrations.ab_cost(x_xyzrgb, y_xyzrgb)
 
-        def plot_cb_gen(output_prefix):
-            def plot_cb(x_nd, y_md, corr_nm, f, iteration):
-                if args.plot_color:
-                    registrations.plot_callback(x_nd, y_md, corr_nm, f, iteration, output_prefix, x_color = x_xyzrgb[:,d:], y_color = y_xyzrgb[:,d:], proj_2d=args.proj)
-                else:
-                    registrations.plot_callback(x_nd, y_md, corr_nm, f, iteration, output_prefix, proj_2d=args.proj)
-            return plot_cb
-
         print "Reg4 EM, w/ visual features"
         f, bend_cost, res_cost, total_cost = registrations.sim_annealing_registration(x_nd, y_md,
                 registrations.reg4_em_step, vis_cost_xy = vis_costs_xy,
-                plotting=1, plot_cb = plot_cb_gen(output_prefix + k + "_reg4vis" if output_prefix else None))
+                plotting=1, plot_cb = plot_cb_gen(os.path.join(args.output_folder, k + "_reg4vis") if args.output_folder else None,
+                                                  args,
+                                                  x_xyzrgb[:,-3:],
+                                                  y_xyzrgb[:,-3:]))
         if rot_degrees > 0:
             if "reg4vis" not in costs:
                 costs['reg4vis'] = []
@@ -74,7 +100,10 @@ def run_experiments(args):
         print "Reg4 EM, w/o visual features"
         f, bend_cost, res_cost, total_cost = registrations.sim_annealing_registration(x_nd, y_md,
                 registrations.reg4_em_step,
-                plotting=1, plot_cb = plot_cb_gen(output_prefix + k + "_reg4" if output_prefix else None))
+                plotting=1, plot_cb = plot_cb_gen(os.path.join(args.output_folder, k + "_reg4vis") if args.output_folder else None,
+                                                  args,
+                                                  x_xyzrgb[:,-3:],
+                                                  y_xyzrgb[:,-3:]))
         if rot_degrees > 0:
             if "reg4" not in costs:
                 costs['reg4'] = []
@@ -83,7 +112,10 @@ def run_experiments(args):
         print "RPM EM, w/ visual features"
         f, bend_cost, res_cost, total_cost = registrations.sim_annealing_registration(x_nd, y_md,
                 registrations.rpm_em_step, vis_cost_xy = vis_costs_xy,
-                plotting=1, plot_cb = plot_cb_gen(output_prefix + k + "_rpmvis" if output_prefix else None))
+                plotting=1, plot_cb = plot_cb_gen(os.path.join(args.output_folder, k + "_reg4vis") if args.output_folder else None,
+                                                  args,
+                                                  x_xyzrgb[:,-3:],
+                                                  y_xyzrgb[:,-3:]))
         if rot_degrees > 0:
             if "rpmvis" not in costs:
                 costs['rpmvis'] = []
@@ -92,22 +124,26 @@ def run_experiments(args):
         print "RPM EM, w/o visual features"
         f, bend_cost, res_cost, total_cost = registrations.sim_annealing_registration(x_nd, y_md,
                 registrations.rpm_em_step,
-                plotting=1, plot_cb = plot_cb_gen(output_prefix + k + "_rpm" if output_prefix else None))
+                plotting=1, plot_cb = plot_cb_gen(os.path.join(args.output_folder, k + "_reg4vis") if args.output_folder else None,
+                                                  args,
+                                                  x_xyzrgb[:,-3:],
+                                                  y_xyzrgb[:,-3:]))
         if rot_degrees > 0:
             if "rpm" not in costs:
                 costs['rpm'] = []
             costs['rpm'].append((rot_degrees, bend_cost, res_cost, total_cost))
         
-        def plot_cb_bij(x_nd, y_md, xtarg_nd, corr_nm, wt_n, f):
-            if args.plot_color:
-                registrations.plot_callback(x_nd, y_md, corr_nm, f, output_prefix + k + "_rpmbij", iteration, res = (.3, .3, .12), x_color = x_xyzrgb[:,d:], y_color = y_xyzrgb[:,d:])
-            else:
-                registrations.plot_callback(x_nd, y_md, corr_nm, f, output_prefix + k + "_rpmbij", iteration, res = (.4, .3, .12))
-        #scaled_x_nd, _ = registration.unit_boxify(x_nd)
-        #scaled_y_md, _ = registration.unit_boxify(y_md)
-        #f,g = registration.tps_rpm_bij(scaled_x_nd, scaled_y_md, plot_cb=plot_cb_bij,
-        #                               plotting=1, rot_reg=np.r_[1e-4, 1e-4, 1e-1][:d], 
-        #                               n_iter=50, reg_init=10, reg_final=.1, outlierfrac=1e-2)
+#         def plot_cb_bij(x_nd, y_md, xtarg_nd, corr_nm, wt_n, f):
+#             iteration = 0 # TODO
+#             if args.plot_color:
+#                 registrations.plot_callback(x_nd, y_md, corr_nm, f, iteration, os.path.join(args.output_folder, k + "_rpmbij"), res = (.3, .3, .12), x_color = x_xyzrgb[:,d:], y_color = y_xyzrgb[:,d:], proj_2d=args.proj)
+#             else:
+#                 registrations.plot_callback(x_nd, y_md, corr_nm, f, iteration, os.path.join(args.output_folder, k + "_rpmbij"), res = (.4, .3, .12), proj_2d=args.proj)
+#         scaled_x_nd, _ = registration.unit_boxify(x_nd)
+#         scaled_y_md, _ = registration.unit_boxify(y_md)
+#         f,g = registration.tps_rpm_bij(scaled_x_nd, scaled_y_md, plot_cb=plot_cb_bij,
+#                                        plotting=1, rot_reg=np.r_[1e-4, 1e-4, 1e-1][:d], 
+#                                        n_iter=50, reg_init=10, reg_final=.1, outlierfrac=1e-2)
         for k in costs:
             cost_vec = costs[k]
             print k
@@ -118,6 +154,7 @@ def run_experiments(args):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", type=str)
+    parser.add_argument("--experiments", type=int, nargs='*', metavar="i_exp", default=[0])
     parser.add_argument("--output_folder", type=str, default="")
     parser.add_argument("--plot_color", type=int, default=1)
     parser.add_argument("--proj", type=int, default=1, help="project 3d visualization into 2d")
